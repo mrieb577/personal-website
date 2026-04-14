05a8435bb164cbf15f0da3185c732bf8f2d46e61
redpanda577 <51419294+mrieb577@users.noreply.github.com>
2026-04-14 07:45:37 -0600

    Remove old java implementation

--- server/src/main/java/com/mrieb/personal_website/Session.java
+++
@@ -1,8 +0,0 @@
-package com.mrieb.personal_website;
-
-import com.mrieb.personal_website.data.*;
-
-public class Session {
-    public static Credentials credentials;
-    public static Authorization authorization;
-}
